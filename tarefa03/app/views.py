0f88f933a3f7c3fb649a934b4ed3a908f7f07ea5
Clara Souza <clarasouzacs07@gmail.com>
2025-06-20 21:36:10 -0300

    tarefa03

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -2,7 +2,42 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
-def index(request):
-    return render(request, "usuarios.html")
+def usuarios(request):
+    usuarioslista = [
+        {
+        "Nome": "Lucas Andrade",
+        "Matrícula": "20251001",
+        "Idade": 22,
+        "Cidade": "Belo Horizonte"
+    },
+    {
+        "Nome": "Marina Costa",
+        "Matrícula": "20251002",
+        "Idade": 25,
+        "Cidade": "São Paulo"
+    },
+    {
+        "Nome": "João Pedro Lima",
+        "Matrícula": "20251003",
+        "Idade": 28,
+        "Cidade": "Curitiba"
+    },
+    {
+        "Nome": "Ana Beatriz Soares",
+        "Matrícula": "20251004",
+        "Idade": 20,
+        "Cidade": "Salvador"
+    },
+    {
+        "Nome": "Rafael Martins",
+        "Matrícula": "20251005",
+        "Idade": 23,
+        "Cidade": "Porto Alegre"
+    }
+    ]
+    context = {
+        "usuarios": usuarioslista,
+    }
+    return render(request, "usuarios.html", context)
 
 # Create your views here.
