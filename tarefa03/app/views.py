04be6b946b29622567154a2fdb92495e2a3a12b0
Clara Souza <clarasouzacs07@gmail.com>
2025-06-20 21:51:49 -0300

    tarefa03

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -6,31 +6,31 @@ def usuarios(request):
     usuarioslista = [
         {
         "Nome": "Lucas Andrade",
-        "Matrícula": "20251001",
+        "Matricula": "20251001",
         "Idade": 22,
         "Cidade": "Belo Horizonte"
     },
     {
         "Nome": "Marina Costa",
-        "Matrícula": "20251002",
+        "Matricula": "20251002",
         "Idade": 25,
         "Cidade": "São Paulo"
     },
     {
         "Nome": "João Pedro Lima",
-        "Matrícula": "20251003",
+        "Matricula": "20251003",
         "Idade": 28,
         "Cidade": "Curitiba"
     },
     {
         "Nome": "Ana Beatriz Soares",
-        "Matrícula": "20251004",
+        "Matricula": "20251004",
         "Idade": 20,
         "Cidade": "Salvador"
     },
     {
         "Nome": "Rafael Martins",
-        "Matrícula": "20251005",
+        "Matricula": "20251005",
         "Idade": 23,
         "Cidade": "Porto Alegre"
     }
